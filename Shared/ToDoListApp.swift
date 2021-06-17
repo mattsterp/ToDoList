9a336c8f6f8d95e3ed28f9a5b59f743727a1cb13
Matthhew Pters <mattsterp@gmail.com>
2021-06-17 10:57:28 -0400

    Initial Commit

---
+++ Shared/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  Shared
+//
+//  Created by Matthew Peters on 6/17/21.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
