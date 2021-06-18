c69faadf1d8098655b14c97596bd19a0d8ddb4ea
Matthhew Pters <mattsterp@gmail.com>
2021-06-18 13:24:51 -0400

    saving data with UserDefaults.

--- Shared/ViewModel/ListViewModel.swift
+++ Shared/ViewModel/ListViewModel.swift
@@ -19,18 +19,24 @@ import Foundation
 
 class ListViewModel: ObservableObject {
     
-    @Published var items: [ItemModel] = []
+    @Published var items: [ItemModel] = [] {
+        didSet {
+            saveItems()
+        }
+    }
+    let itemsKey: String = "item_list"
     
     init() {
         getItems()
     }
     func getItems() {
-        let newItems = [
-            ItemModel(title: "This is the first title", isCompleted: false),
-            ItemModel(title: "This is the second title", isCompleted: true),
-            ItemModel(title: "This is the third title", isCompleted: false),
-        ]
-        items.append(contentsOf: newItems)
+        
+        guard
+            let data = UserDefaults.standard.data(forKey: itemsKey),
+            let savedItems = try? JSONDecoder().decode([ItemModel].self, from: data)
+        else { return }
+        
+        self.items = savedItems
     }
     
     func deleteItem(indexSet: IndexSet) {
@@ -47,15 +53,14 @@ class ListViewModel: ObservableObject {
     }
     
     func updateItem(item: ItemModel) {
-        
-//         if let index = items.firstIndex { (existingItem) -> Bool in
-//            return existingItem.id == item.id
-//         } {
-//            // run this code
-//         }
-        
         if let index = items.firstIndex(where: { $0.id == item.id }) {
             items[index] = item.updateCompletion()
+        }
+    }
+    
+    func saveItems() {
+        if let encodedData = try? JSONEncoder().encode(items) {
+            UserDefaults.standard.set(encodedData, forKey: itemsKey)
         }
     }
 }
