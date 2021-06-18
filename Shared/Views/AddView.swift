b0a10c4b69e944649c5f0fb14abdae94205dff8d
Matthhew Pters <mattsterp@gmail.com>
2021-06-18 12:26:40 -0400

    implemented the ViewModel

--- Shared/Views/AddView.swift
+++ Shared/Views/AddView.swift
@@ -9,7 +9,11 @@ import SwiftUI
 
 struct AddView: View {
     
+    @Environment(\.presentationMode) var presentationMode
+    @EnvironmentObject var listViewModel: ListViewModel
     @State var textFieldText: String = ""
+    @State var alertTitle: String = ""
+    @State var showAlert: Bool = false
     
     var body: some View {
         ScrollView {
@@ -19,9 +23,7 @@ struct AddView: View {
                     .frame(height: 55)
                     .background(Color(#colorLiteral(red: 0.921431005, green: 0.9214526415, blue: 0.9214410186, alpha: 1)))
                     .cornerRadius(10)
-                Button(action: {
-                    
-                }, label: {
+                Button(action: saveButtonPressed, label: {
                     Text("Save".uppercased())
                         .foregroundColor(.white)
                         .font(.headline)
@@ -33,6 +35,28 @@ struct AddView: View {
             .padding(14)
         }
         .navigationTitle("Add an Item 🖊")
+        .alert(isPresented: $showAlert, content: getAlert)
+    }
+    
+    func saveButtonPressed(){
+        if textIsAppropriate(){
+            listViewModel.addItem(title: textFieldText)
+            presentationMode.wrappedValue.dismiss()
+        }
+    }
+    
+    func textIsAppropriate() -> Bool {
+        if textFieldText.count < 3 {
+            alertTitle = "Your new To Do item must be at least 3 characters long!😬"
+            showAlert.toggle()
+            return false
+        }
+        return true
+    }
+    
+    
+    func getAlert() -> Alert {
+        return Alert(title: Text(alertTitle))
     }
 }
 
@@ -41,5 +65,6 @@ struct AddView_Previews: PreviewProvider {
         NavigationView {
             AddView()
         }
+        .environmentObject(ListViewModel())
     }
 }
